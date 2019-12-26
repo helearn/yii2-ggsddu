8c94ebd4b5c3578894862019d5e6e868673eae22
Helearn <helearn@aliyun.com>
2019-12-26 14:51:37 +0800

    业余开发者，别拿去学习

--- Module.php
+++ Module.php
@@ -163,7 +163,8 @@ class Module extends \yii\base\Module
             $view = $action->controller->getView();
 
             $view->params['breadcrumbs'][] = [
-                'label' => ($this->defaultUrlLabel ?: Yii::t('rbac-admin', 'Admin')),
+                //'label' => ($this->defaultUrlLabel ?: Yii::t('helearn-yii2ggsddu', 'Knowledge')),
+                'label' => ($this->defaultUrlLabel ?:  'Knowledge'),
                 'url' => ['/' . ($this->defaultUrl ?: $this->uniqueId)],
             ];
             return true;
